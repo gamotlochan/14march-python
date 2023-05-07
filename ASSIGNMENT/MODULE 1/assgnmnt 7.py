bd8366e8c308b138f474451c34dcbc02a5e156d3
LochanGamot <127671749+LochanGamot@users.noreply.github.com>
2023-05-07 11:20:34 +0530

    Create assgnmnt 7.py

---
+++ ASSIGNMENT/MODULE 1/assgnmnt 7.py
@@ -0,0 +1,9 @@
+#7 program to find greatest common divisor of two numbers.
+#for example, GCD of 20 and is 4 and GCD of 98 and 56 is 14.
+
+gcd="greatest common divisor"
+
+import math
+
+print("the gcd of 60 and 48 is : ",end="")
+print(math.gcd(60,48))

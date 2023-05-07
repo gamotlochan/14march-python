9a5f5e4fcb2dd700a87bd32f47644ab8ff6cc707
LochanGamot <127671749+LochanGamot@users.noreply.github.com>
2023-05-07 11:19:18 +0530

    Create assgnmnt 3.py

---
+++ ASSIGNMENT/MODULE 1/assgnmnt 3.py
@@ -0,0 +1,9 @@
+# 3 write a python program to count the occurrences of each word in a given sentence.
+
+str="hello welcome to a python"
+str=str.count()
+def word_count(str):
+    
+            
+    print(str.count())
+            
